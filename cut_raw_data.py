4ed1ba76c05fee9d755bbd9adf2d206d70b7c169
denise-hvl <144991036+denise-hvl@users.noreply.github.com>
2023-09-14 11:14:16 +0200

    Update cut_raw_data.py

--- cut_raw_data.py
+++ cut_raw_data.py
@@ -1,29 +1,43 @@
 
 import geopandas as gpd
-
+import pandas as pd
 
 
 if __name__ == '__main__':
     #load the full database
-    #
-    path = r"C:\Users\cda055\OneDrive - UiT Office 365\Impetus\Slushflow_db\NVE_60751B14_1683888916789_12228\NVEData\Skred_Skredhendelse"
-    db_file = path + "\Skred_Skredhendelse.shp" # add the file name to the path
-    point_data = gpd.read_file(db_file) # read the file as a dataframe
+    path = r"C:\Users\deniser\OneDrive - Høgskulen på Vestlandet\artikler\2024 - hans\GIS\Pro\nasjonal skreddatabase\Skred_Skredhendelse"
+    
+    # add the file name to the path
+    db_file = path + "\Skred_Skredhendelse.shp" 
+    
+    # read the file as a dataframe
+    point_data = gpd.read_file(db_file) 
 
-    slushflows = point_data[point_data['skredType'] == 133] # find points in colum skredType with tag 133 which are the recoarded slushflow events
-    unique_rows = slushflows['skredID'].nunique() # Find unique ID values for the skredID that can be searched in the two other data files
-    rows = slushflows["skredID"] # make a list of the skredIDs so we can search the other database for these events
+    #define time period
+    date1 = pd.to_datetime('2023-08-06')
+    date2 = pd.to_datetime('2023-08-12')
 
-    #load polygone and release pt data
+    # pointdata = the dataframe before it is cut for the dates, df is the dataframe after
+    df = pointdata.loc[(gdf['date'] >= date1) & (gdf['date'] <= date2)]
+
+    # find points in column skredType with tag either 140 (Løsmassedskred, uspesifisert), 142 (Flomskred), 143 (Leirskred), 144 (Jordskred) or 145 (Jordskred, uspesifisert)
+    losmasseskred = df[point_data[{'skredType'== 140}, {'skredType'== 142}, {'skredType'== 143}, {'skredType'== 144}, {'skredType'== 145}] 
+
+    # Find unique ID values for the skredID that can be searched in the two other data files
+    unique_rows = losmasseskred['skredID'].nunique() 
+    
+    # make a list of the skredIDs so we can search the other database for these events
+    rows = losmasseskred["skredID"] 
+
+    #load polygon and release point data
     polygon_data = gpd.read_file(path + "\Skred_Skredhendelse_UtlopUtlosningOmr.shp")
     subset_poly = polygon_data[polygon_data['skredID'].isin(rows)]
 
-    release_data = gpd.read_file(path + "\Skred_Skredhendelse_UtlopUtlosningPkt.shp")
-    subset_release = polygon_data[polygon_data['skredID'].isin(rows)]
-
-    # these lines are used to save data files consisting of only slushflows for loading in QGIS
-    subset_release.to_file(path + '\subset_release.shp', driver='ESRI Shapefile')
-    subset_poly.to_file(path + '\subset_poly.shp', driver='ESRI Shapefile')
-    slushflows.to_file(path + '\slushflows.shp', driver='ESRI Shapefile')
+    release_point_data = gpd.read_file(path + "\Skred_Skredhendelse_UtlopUtlosningPkt.shp")
+    subset_releasepoint = release_point_data[release_point_data['skredID'].isin(rows)]
 
 
+    # these lines are used to save data files consisting of only losmasseskred and a given time periode (Hans) for loading in GIS
+    subset_releasepoint.to_file(path + '\losmasse_hans_startpunkt.shp', driver='ESRI Shapefile')
+    subset_poly.to_file(path + '\losmasse_hans_polygon.shp', driver='ESRI Shapefile')
+    slushflows.to_file(path + '\losmasseskred_hans.shp', driver='ESRI Shapefile')
